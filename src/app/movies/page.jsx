794f0c7727552dd9f9c69ea92f58a522aea9a9c0
Carla <c.nadaligdutto@itecriocuarto.org.ar>
2026-06-07 21:40:19 -0300

    Creacion Layout anidado en /Movies

---
+++ src/app/movies/page.jsx
@@ -0,0 +1,9 @@
+export default function page() {
+  return (
+    <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 dark:bg-black">
+      <section className="">
+        <h1>peli</h1>
+      </section>
+    </main>
+  )
+}
